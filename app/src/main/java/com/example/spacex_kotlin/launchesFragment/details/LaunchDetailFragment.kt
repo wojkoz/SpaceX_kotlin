502ca2833d3ffcd0fa0acdef937806833c9fe13c
TheEnter3 <wkoziol3ib@gmail.com>
2020-01-14 20:44:54 +0100

    fixed events loads at first startup

--- app/src/main/java/com/example/spacex_kotlin/launchesFragment/details/LaunchDetailFragment.kt
+++ app/src/main/java/com/example/spacex_kotlin/launchesFragment/details/LaunchDetailFragment.kt
@@ -58,5 +58,5 @@ class LaunchDetailFragment : Fragment() {
 
 
     }
-    
+
 }
--- app/src/main/java/com/example/spacex_kotlin/rocketsFragment/details/RocketDetailViewModel.kt
+++ app/src/main/java/com/example/spacex_kotlin/rocketsFragment/details/RocketDetailViewModel.kt
@@ -2,20 +2,18 @@ package com.example.spacex_kotlin.rocketsFragment.details
 
 
 
-import androidx.lifecycle.*
-import com.example.spacex_kotlin.utils.LoadingState
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.spacex_kotlin.repository.SpacexRepository
-import com.example.spacex_kotlin.repository.model.retrofit.Rocket
 import com.example.spacex_kotlin.repository.model.room.rocket.RocketDetail
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 
-class RocketDetailViewModel(id: String, repo: SpacexRepository) : ViewModel() {
+class RocketDetailViewModel(private val id: String, private val repo: SpacexRepository) : ViewModel() {
 
-    val _data = MediatorLiveData<RocketDetail>()
+    private val _data = MediatorLiveData<RocketDetail>()
     val data: LiveData<RocketDetail>
         get() = _data
 
